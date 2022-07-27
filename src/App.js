682ea1c3b488c29f2f882f23bf2783b331fe06b2
jdawsonimgix <jdawson@imgix.com>
2022-07-27 14:27:24 -0700

    start

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import './App.css'
+import * as React from 'react'
+
+import VideoJS from './VideoJS'
+import { createComponent } from '@lit-labs/react'
+import { IxVideo } from '@imgix/ix-video'
+
+export const ImgixVideoPlayer = createComponent(React, 'ix-video', IxVideo, {
+  onSeeked: 'seeked',
+})
+
+function App() {
+  const videoJsOptions = {
+    autoplay: false,
+    controls: true,
+    sources: [
+      {
+        src:
+          'https://assets.imgix.video/videos/girl-reading-book-in-library.mp4',
+        type: 'video/mp4',
+      },
+    ],
+  }
+
+  return (
+    <div className="app">
+      <VideoJS options={videoJsOptions} />
+      <ImgixVideoPlayer
+        controls
+        width={600}
+        source="https://assets.imgix.video/videos/girl-reading-book-in-library.mp4"
+        onSeeked={(e) => console.log(e)}
+      />
+    </div>
+  )
+}
+
+export default App
